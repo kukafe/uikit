b1e692447df4a67e15245f21f5d583e005e10c04
name <email>
2021-07-11 22:34:53 +0800

    v1

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2195,16 +2195,16 @@ var links = [
             // },
             {
                 label: "Github",
-                href: "https://github.com/goosedefi/",
+                href: "https://github.com/kukafe/",
             },
             {
                 label: "Docs",
-                href: "https://goosedefi.gitbook.io/goose-finance/",
+                href: "https://kukafe.gitbook.io/kukafe",
             },
-            {
-                label: "Blog",
-                href: "https://goosefinance.medium.com/",
-            },
+            // {
+            //   label: "Blog",
+            //   href: "https://goosefinance.medium.com/",
+            // },
         ],
     },
 ];
@@ -2215,68 +2215,68 @@ var socials = [
         items: [
             {
                 label: "English",
-                href: "https://t.me/goosedefi",
+                href: "https://t.me/kukafe",
             },
             // {
             //   label: "Bahasa Indonesia",
             //   href: "https://t.me/PancakeSwapIndonesia",
             // },
-            {
-                label: "中文",
-                href: "https://t.me/goosefinancechinese",
-            },
+            // {
+            //   label: "中文",
+            //   href: "https://t.me/goosefinancechinese",
+            // },
             // {
             //   label: "Tiếng Việt",
             //   href: "https://t.me/PancakeSwapVN",
             // },
-            {
-                label: "Italiano",
-                href: "https://t.me/goosefinanceitalian",
-            },
-            {
-                label: "русский",
-                href: "https://t.me/goosefinancerussian",
-            },
-            {
-                label: "Türkiye",
-                href: "https://t.me/GooseFinanceTurkey",
-            },
+            // {
+            //   label: "Italiano",
+            //   href: "https://t.me/goosefinanceitalian",
+            // },
+            // {
+            //   label: "русский",
+            //   href: "https://t.me/goosefinancerussian",
+            // },
+            // {
+            //   label: "Türkiye",
+            //   href: "https://t.me/GooseFinanceTurkey",
+            // },
             // {
             //   label: "Português",
             //   href: "https://t.me/PancakeSwapPortuguese",
             // },
-            {
-                label: "Español",
-                href: "https://t.me/goosefinancespanish",
-            },
-            {
-                label: "日本語",
-                href: "https://t.me/goosefinancejapanese",
-            },
-            {
-                label: "Français",
-                href: "https://t.me/goosefinancefrench",
-            },
-            {
-                label: "Announcements",
-                href: "https://t.me/goosefinanceann",
-            },
-            {
-                label: "Price Bot",
-                href: "https://t.me/BinanceRocketEGG",
-            },
+            // {
+            //   label: "Español",
+            //   href: "https://t.me/goosefinancespanish",
+            // },
+            // {
+            //   label: "日本語",
+            //   href: "https://t.me/goosefinancejapanese",
+            // },
+            // {
+            //   label: "Français",
+            //   href: "https://t.me/goosefinancefrench",
+            // },
+            // {
+            //   label: "Announcements",
+            //   href: "https://t.me/goosefinanceann",
+            // },
+            // {
+            //   label: "Price Bot",
+            //   href: "https://t.me/BinanceRocketEGG",
+            // },
         ],
     },
     {
         label: "Twitter",
         icon: "TwitterIcon",
-        href: "https://twitter.com/goosedefi",
+        href: "https://twitter.com/kukafe2",
     },
-    {
-        label: "Reddit",
-        icon: "RedditIcon",
-        href: "https://www.reddit.com/r/GooseFinanceofficial",
-    },
+    // {
+    //   label: "Reddit",
+    //   icon: "RedditIcon",
+    //   href: "https://www.reddit.com/r/GooseFinanceofficial",
+    // },
 ];
 var MENU_HEIGHT = 64;
 var MENU_ENTRY_HEIGHT = 48;
--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -99,16 +99,16 @@ export const links = [
       // },
       {
         label: "Github",
-        href: "https://github.com/goosedefi/",
+        href: "https://github.com/kukafe/",
       },
       {
         label: "Docs",
-        href: "https://goosedefi.gitbook.io/goose-finance/",
+        href: "https://kukafe.gitbook.io/kukafe",
       },
-      {
-        label: "Blog",
-        href: "https://goosefinance.medium.com/",
-      },
+      // {
+      //   label: "Blog",
+      //   href: "https://goosefinance.medium.com/",
+      // },
     ],
   },
 ];
@@ -120,68 +120,68 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/goosedefi",
+        href: "https://t.me/kukafe",
       },
       // {
       //   label: "Bahasa Indonesia",
       //   href: "https://t.me/PancakeSwapIndonesia",
       // },
-      {
-        label: "中文",
-        href: "https://t.me/goosefinancechinese",
-      },
+      // {
+      //   label: "中文",
+      //   href: "https://t.me/goosefinancechinese",
+      // },
       // {
       //   label: "Tiếng Việt",
       //   href: "https://t.me/PancakeSwapVN",
       // },
-      {
-        label: "Italiano",
-        href: "https://t.me/goosefinanceitalian",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/goosefinancerussian",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/GooseFinanceTurkey",
-      },
+      // {
+      //   label: "Italiano",
+      //   href: "https://t.me/goosefinanceitalian",
+      // },
+      // {
+      //   label: "русский",
+      //   href: "https://t.me/goosefinancerussian",
+      // },
+      // {
+      //   label: "Türkiye",
+      //   href: "https://t.me/GooseFinanceTurkey",
+      // },
       // {
       //   label: "Português",
       //   href: "https://t.me/PancakeSwapPortuguese",
       // },
-      {
-        label: "Español",
-        href: "https://t.me/goosefinancespanish",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/goosefinancejapanese",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/goosefinancefrench",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/goosefinanceann",
-      },
-      {
-        label: "Price Bot",
-        href: "https://t.me/BinanceRocketEGG",
-      },
+      // {
+      //   label: "Español",
+      //   href: "https://t.me/goosefinancespanish",
+      // },
+      // {
+      //   label: "日本語",
+      //   href: "https://t.me/goosefinancejapanese",
+      // },
+      // {
+      //   label: "Français",
+      //   href: "https://t.me/goosefinancefrench",
+      // },
+      // {
+      //   label: "Announcements",
+      //   href: "https://t.me/goosefinanceann",
+      // },
+      // {
+      //   label: "Price Bot",
+      //   href: "https://t.me/BinanceRocketEGG",
+      // },
     ],
   },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/goosedefi",
+    href: "https://twitter.com/kukafe2",
   },
-  {
-    label: "Reddit",
-    icon: "RedditIcon",
-    href: "https://www.reddit.com/r/GooseFinanceofficial",
-  },
+  // {
+  //   label: "Reddit",
+  //   icon: "RedditIcon",
+  //   href: "https://www.reddit.com/r/GooseFinanceofficial",
+  // },
 ];
 
 export const MENU_HEIGHT = 64;
