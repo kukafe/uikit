c2d62aaff021b28f17ea951244455c060eaff27a
name <email>
2021-07-11 22:38:37 +0800

    v1

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -37,7 +37,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#888888",
+  secondary: "##ffcb2f",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
