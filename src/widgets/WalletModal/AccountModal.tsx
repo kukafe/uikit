abb260d8c7c42ee5ad75eb17959b71dd128b2401
name <email>
2021-07-17 11:22:33 +0800

    b1

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
+      <LinkExternal small href={`https://explorer.kcc.io/en/address/${account}`} mr="16px">
+        View on KccExplorer
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
