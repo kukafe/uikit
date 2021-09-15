fd03c244be942c493e9825c2649f26aacf495b7f
name <email>
2021-09-15 12:46:25 +0800

    1

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2618,10 +2618,13 @@ var templateObject_1$5, templateObject_2$2;
 
 var AccountModal = function (_a) {
     var account = _a.account, logout = _a.logout, _b = _a.onDismiss, onDismiss = _b === void 0 ? function () { return null; } : _b;
+    var kccMode = window.location.href.indexOf('ku.kafe.finance') >= 0 || window.location.href.indexOf('kukafe.finance') >= 0;
+    var movrMode = window.location.href.indexOf('moon.kafe.finance') >= 0;
     return (React.createElement(Modal, { title: "Your wallet", onDismiss: onDismiss },
         React.createElement(Text, { fontSize: "20px", bold: true, style: { whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" } }, account),
         React.createElement(Flex, { mb: "32px" },
-            React.createElement(LinkExternal, { small: true, href: "https://explorer.kcc.io/en/address/" + account, mr: "16px" }, "View on KccExplorer"),
+            movrMode ? (React.createElement(LinkExternal, { small: true, href: "https://blockscout.moonriver.moonbeam.network/address/" + account, mr: "16px" }, "View on BlockScout")) : (""),
+            kccMode ? (React.createElement(LinkExternal, { small: true, href: "https://explorer.kcc.io/en/address/" + account, mr: "16px" }, "View on KccExplorer")) : (""),
             React.createElement(CopyToClipboard, { toCopy: account }, "Copy Address")),
         React.createElement(Flex, { justifyContent: "center" },
             React.createElement(Button, { size: "sm", variant: "secondary", onClick: function () {
--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -13,36 +13,58 @@ interface Props {
   onDismiss?: () => void;
 }
 
-const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null }) => (
-  <Modal title="Your wallet" onDismiss={onDismiss}>
-    <Text
-      fontSize="20px"
-      bold
-      style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" }}
-    >
-      {account}
-    </Text>
-    <Flex mb="32px">
-      <LinkExternal small href={`https://explorer.kcc.io/en/address/${account}`} mr="16px">
-        View on KccExplorer
-      </LinkExternal>
-      <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
-    </Flex>
-    <Flex justifyContent="center">
-      <Button
-        size="sm"
-        variant="secondary"
-        onClick={() => {
-          logout();
-          window.localStorage.removeItem(localStorageKey);
-          onDismiss();
-          window.location.reload();
-        }}
+const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null }) => {
+
+  const kccMode = window.location.href.indexOf('ku.kafe.finance') >= 0 || window.location.href.indexOf('kukafe.finance') >= 0
+  const movrMode = window.location.href.indexOf('moon.kafe.finance') >= 0
+
+  return (
+    <Modal title="Your wallet" onDismiss={onDismiss}>
+      <Text
+        fontSize="20px"
+        bold
+        style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" }}
       >
-        Logout
-      </Button>
-    </Flex>
-  </Modal>
-);
+        {account}
+      </Text>
+      <Flex mb="32px">
+        {/* <LinkExternal small href={`https://explorer.kcc.io/en/address/${account}`} mr="16px">
+          View on {movrMode ? "BlockScout":"KccExplorer"}
+        </LinkExternal> */}
+
+        {movrMode ? (<LinkExternal small href={`https://blockscout.moonriver.moonbeam.network/address/${account}`} mr="16px">
+          View on BlockScout
+        </LinkExternal>):("")}
+
+
+          {kccMode ? (<LinkExternal small href={`https://explorer.kcc.io/en/address/${account}`} mr="16px">
+          View on KccExplorer
+        </LinkExternal>):("")}
+
+
+        <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
+      </Flex>
+      <Flex justifyContent="center">
+        <Button
+          size="sm"
+          variant="secondary"
+          onClick={() => {
+            logout();
+            window.localStorage.removeItem(localStorageKey);
+            onDismiss();
+            window.location.reload();
+          }}
+        >
+          Logout
+        </Button>
+      </Flex>
+    </Modal>
+  );
+}
+
+
+
+
+
 
 export default AccountModal;
